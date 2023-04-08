cf97e6902c1ad5083ba43c4d0f9fe0572b84a58b
Trevor Vaughn Hauck <hauck.trevor@gmail.com>
2023-04-08 11:17:36 -0400

    Dynamic rendering empty hour blocks or duration blocks, with respective colors, title only on first block.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -663,14 +663,14 @@ const Calendar: Component = () => {
     const [hours, _setHours] = createSignal<number[]>([...Array(24).keys()]);
     const [currDay, _setCurrDay] = createSignal(new Date().getDay());
     const [currHour, _setCurrHour] = createSignal(new Date().getHours());
-    const [durations, setDurations] = createSignal<Duration[]>([
+    const [durations, _setDurations] = createSignal<Duration[]>([
         {
             id: 0,
             category_id: 17,
-            day_as_int: 3,
+            day_as_int: 1,
             description: "",
             category_name: "Work",
-            title: "my first duration",
+            title: "work",
             start_hour: "9",
             start_min: "00",
             end_hour: "17",
@@ -691,7 +691,7 @@ const Calendar: Component = () => {
 
     onMount(() => {
         setTimeout(() => {
-            window.scrollTo(0, currHour() * 100);
+            window.scrollTo(0, currHour() * 60);
         }, 150);
     });
 
@@ -701,140 +701,203 @@ const Calendar: Component = () => {
 
     return (
         <div>
-            <div class={styles.calendarContainerOuter}>
-                <div class={styles.calendarContainer}>
-                    <div></div>
-                    {days().map((day, index) => (
-                        <div class={styles.calheader}>{day}</div>
-                    ))}
-                </div>
-            </div>
-            <div class={styles.spacer}></div>
-
-            <div class={styles.calendarContainer}>
-                <div class={styles.calendarHoursContainer}>
-                    {hours()!.map((hour) => (
-                        <div>{hour}</div>
-                    ))}
-                </div>
+            <div class={styles.daysNamesContainer}>
+                <span></span>
                 {days().map((day, index) => (
-                    <div class={styles.calheader}>
-                        <div class={styles.calendarHoursContainer}>
-                            {hours()!.map((hour, hourIndex) => {
-                                if (
-                                    parseInt(durations()[0].start_hour, 10) === hour ||
-                                    parseInt(durations()[0].end_hour, 10) - 1 === hour ||
-                                    (hour > parseInt(durations()[0].start_hour, 10) &&
-                                        hour < parseInt(durations()[0].end_hour, 10) - 1)
-                                ) {
-                                    return (
-                                        <div
-                                            id={
-                                                currHour() - 1 === hourIndex && currDay() === index
-                                                    ? "scrollTo"
-                                                    : ""
-                                            }
-                                            class={`${styles.hourBlock} ${
-                                                currDay() === index ? styles.hourBlockCurrDay : ""
-                                            } ${
-                                                currHour() === hourIndex && currDay() === index
-                                                    ? styles.hourBlockCurrHour
-                                                    : ""
-                                            }`}
-                                        >
-                                            {" "}
-                                            <div class={styles.hourBlockContent}>
-                                                {/* {hour}:00 */}
-                                                <div
-                                                    class={styles.eventBlock}
-                                                    style={{
-                                                        "background-color": durations()[0].color,
-                                                        opacity: "0.8",
-                                                    }}
-                                                >
-                                                    <div class={styles.hourBlockInner}>
-                                                        {parseInt(durations()[0].start_hour, 10) ===
-                                                        hour
-                                                            ? durations()[0].title
-                                                            : ""}
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    );
-                                }
-
-                                return (
-                                    <div
-                                        id={
-                                            currHour() - 1 === hourIndex && currDay() === index
-                                                ? "scrollTo"
-                                                : ""
-                                        }
-                                        class={`${styles.hourBlock} ${
-                                            currDay() === index ? styles.hourBlockCurrDay : ""
-                                        } ${
-                                            currHour() === hourIndex && currDay() === index
-                                                ? styles.hourBlockCurrHour
-                                                : ""
-                                        }`}
-                                    >
-                                        <div class={styles.hourBlockContent}>
-                                            <div>
-                                                <div class={styles.hourBlockInner}>
-                                                    {parseInt(durations()[0].start_hour, 10) ===
-                                                    hour
-                                                        ? durations()[0].title
-                                                        : ""}
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                );
-                            })}
-                        </div>
+                    <div
+                        class={`${styles.calheader} ${
+                            currDay() === index ? styles.hourBlockCurrDay : ""
+                        }`}
+                    >
+                        {day}
                     </div>
                 ))}
             </div>
 
-            <div
-                onClick={handleAddCalEvent}
-                class={styles.addCalEventButton}
-            >
-                <svg
-                    fill="#46daff"
-                    height="50px"
-                    width="50px"
-                    version="1.1"
-                    id="Layer_1"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 300.003 300.003"
-                >
-                    <g
-                        id="SVGRepo_bgCarrier"
-                        stroke-width="0"
-                    ></g>
-                    <g
-                        id="SVGRepo_tracerCarrier"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                    ></g>
-                    <g id="SVGRepo_iconCarrier">
-                        {" "}
-                        <g>
-                            {" "}
-                            <g>
-                                {" "}
-                                <path d="M150,0C67.159,0,0.001,67.159,0.001,150c0,82.838,67.157,150.003,149.997,150.003S300.002,232.838,300.002,150 C300.002,67.159,232.839,0,150,0z M213.281,166.501h-48.27v50.469c-0.003,8.463-6.863,15.323-15.328,15.323 c-8.468,0-15.328-6.86-15.328-15.328v-50.464H87.37c-8.466-0.003-15.323-6.863-15.328-15.328c0-8.463,6.863-15.326,15.328-15.328 l46.984,0.003V91.057c0-8.466,6.863-15.328,15.326-15.328c8.468,0,15.331,6.863,15.328,15.328l0.003,44.787l48.265,0.005 c8.466-0.005,15.331,6.86,15.328,15.328C228.607,159.643,221.742,166.501,213.281,166.501z"></path>{" "}
-                            </g>{" "}
-                        </g>{" "}
-                    </g>
-                </svg>
+            <div class={styles.calContentContainer}>
+                <div class={styles.hoursContainer}>
+                    {hours()!.map((hour) => (
+                        <div>
+                            <div>{hour}</div>
+                        </div>
+                    ))}
+                </div>
+                {days().map((_, dayIndex) => {
+                    return (
+                        <div class={styles.daysContainer}>
+                            {hours().map((_, hourIndex) => {
+                                const { isMatch, dur, display } = isDurationThisDayThisHour(
+                                    durations(),
+                                    dayIndex,
+                                    hourIndex,
+                                );
+
+                                if (isMatch && dur) {
+                                    return (
+                                        <EmptyHourBlock
+                                            currDay={currDay()}
+                                            currHour={currHour()}
+                                            hourIndex={hourIndex}
+                                            dayIndex={dayIndex}
+                                        >
+                                            <DurationHourBlock
+                                                currDay={currDay()}
+                                                currHour={currHour()}
+                                                hourIndex={hourIndex}
+                                                dayIndex={dayIndex}
+                                                title={dur.title}
+                                                shouldDisplayTitle={display}
+                                                color={dur.color}
+                                            />
+                                        </EmptyHourBlock>
+                                    );
+                                }
+                                return (
+                                    <EmptyHourBlock
+                                        currDay={currDay()}
+                                        currHour={currHour()}
+                                        hourIndex={hourIndex}
+                                        dayIndex={dayIndex}
+                                    />
+                                );
+                            })}
+                        </div>
+                    );
+                })}
+            </div>
+
+            <AddCalEventButton addCalEvent={handleAddCalEvent} />
+        </div>
+    );
+};
+
+const isDurationThisDayThisHour = (durations: Duration[], currDay: number, currHour: number) => {
+    for (const i in durations) {
+        const startHour = parseInt(durations[i].start_hour, 10);
+        const endHour = parseInt(durations[i].end_hour, 10);
+
+        if (durations[i].day_as_int === currDay) {
+            if (startHour === currHour) {
+                return {
+                    isMatch: true,
+                    dur: durations[i],
+                    display: true,
+                };
+            }
+            if (endHour - 1 === currHour) {
+                return {
+                    isMatch: true,
+                    dur: durations[i],
+                    display: false,
+                };
+            }
+            if (currHour > startHour && currHour < endHour) {
+                return {
+                    isMatch: true,
+                    dur: durations[i],
+                    display: false,
+                };
+            }
+        }
+    }
+    return {
+        isMatch: false,
+        dur: null,
+        display: false,
+    };
+};
+
+const DurationHourBlock: Component<{
+    currHour: number;
+    hourIndex: number;
+    currDay: number;
+    dayIndex: number;
+    title: string;
+    shouldDisplayTitle: boolean;
+    color: string;
+}> = (props) => {
+    const { currHour, hourIndex, currDay, dayIndex, title, shouldDisplayTitle, color } = props;
+    return (
+        <div
+            id={currHour - 1 === hourIndex && currDay === dayIndex ? "scrollTo" : ""}
+            class={`${styles.hourBlock} ${currDay === dayIndex ? styles.hourBlockCurrDay : ""} ${
+                currHour === hourIndex && currDay === dayIndex ? styles.hourBlockCurrHour : ""
+            }`}
+            style={{
+                "background-color": color,
+                opacity: "0.8",
+            }}
+        >
+            <div class={styles.hourBlockContent}>
+                <div>
+                    <div class={styles.hourBlockInner}>{shouldDisplayTitle ? title : ""}</div>
+                </div>
             </div>
         </div>
     );
 };
 
+const EmptyHourBlock: ParentComponent<{
+    currHour: number;
+    hourIndex: number;
+    currDay: number;
+    dayIndex: number;
+}> = (props) => {
+    const { currHour, hourIndex, currDay, dayIndex } = props;
+    return (
+        <div
+            id={currHour - 1 === hourIndex && currDay === dayIndex ? "scrollTo" : ""}
+            class={`${styles.hourBlock} ${currDay === dayIndex ? styles.hourBlockCurrDay : ""} ${
+                currHour === hourIndex && currDay === dayIndex ? styles.hourBlockCurrHour : ""
+            }`}
+        >
+            <div class={styles.hourBlockContent}>
+                <div>
+                    <div class={styles.hourBlockInner}>{props.children}</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const AddCalEventButton: Component<{ addCalEvent: CallableFunction }> = (props) => {
+    return (
+        <div
+            onClick={() => props.addCalEvent()}
+            class={styles.addCalEventButton}
+        >
+            <svg
+                fill="#46daff"
+                height="50px"
+                width="50px"
+                version="1.1"
+                id="Layer_1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 300.003 300.003"
+            >
+                <g
+                    id="SVGRepo_bgCarrier"
+                    stroke-width="0"
+                ></g>
+                <g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                    {" "}
+                    <g>
+                        {" "}
+                        <g>
+                            {" "}
+                            <path d="M150,0C67.159,0,0.001,67.159,0.001,150c0,82.838,67.157,150.003,149.997,150.003S300.002,232.838,300.002,150 C300.002,67.159,232.839,0,150,0z M213.281,166.501h-48.27v50.469c-0.003,8.463-6.863,15.323-15.328,15.323 c-8.468,0-15.328-6.86-15.328-15.328v-50.464H87.37c-8.466-0.003-15.323-6.863-15.328-15.328c0-8.463,6.863-15.326,15.328-15.328 l46.984,0.003V91.057c0-8.466,6.863-15.328,15.326-15.328c8.468,0,15.331,6.863,15.328,15.328l0.003,44.787l48.265,0.005 c8.466-0.005,15.331,6.86,15.328,15.328C228.607,159.643,221.742,166.501,213.281,166.501z"></path>{" "}
+                        </g>{" "}
+                    </g>{" "}
+                </g>
+            </svg>
+        </div>
+    );
+};
 const ScheduleEditor: Component = () => {
     const [startHour, setStartHour] = createSignal<string>("0");
     const [startMin, setStartMin] = createSignal<string>("00");
@@ -899,7 +962,7 @@ const App: Component = () => {
     return (
         <div class={styles.App}>
             <div class={styles.Navbar}>
-                <A href="/">Chaos Manager</A> | <A href="/calendar">Calendar</A>
+                <A href="/">Chaos Manager</A> | <A href="/focus">Focus</A>
             </div>
             <div class={styles.spacer}></div>
             <Routes>
@@ -908,7 +971,7 @@ const App: Component = () => {
                     component={Home}
                 />
                 <Route
-                    path="/calendar"
+                    path="/focus"
                     component={Calendar}
                 />
                 <Route
